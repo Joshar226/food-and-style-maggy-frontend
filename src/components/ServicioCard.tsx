7fc0278efb0f50b338cd78439b888d899d5fec4f
Joshar226 <josharaya226@gmail.com>
2025-09-16 14:18:12 -0600

    ServiciosCard update

--- src/components/ServicioCard.tsx
+++ src/components/ServicioCard.tsx
@@ -16,13 +16,13 @@ export default function ServicioCard({servicio} : ServicioCardProps) {
       className="bg-[#FFD3C8] px-8 py-5 rounded-lg flex flex-col items-start gap-3 justify-center shadow-xl"
     >
       <div className="bg-white p-2 rounded-full" >
-        <div className="w-5 h-5 text-[#a42424]">{servicio.icono}</div>
+        <div className="w-5 h-5 text-[#FF6D4A]">{servicio.icono}</div>
       </div>
       <h3 className="C_Montserrat font-semibold">{servicio.titulo}</h3>
       <p>{servicio.descripcion}</p>
       <Link 
         to={'/servicios'}
-        className="uppercase text-white font-semibold"  
+        className="uppercase text-[#FF6D4A] font-semibold"  
       >Ver más</Link>
     </div>
   )
